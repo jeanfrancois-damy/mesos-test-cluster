434c66526afd8d42a64b7bd6c1c0991e9ebdb4e6
jeff <jeff@studiogazoline.com>
2016-03-06 11:52:04 +0100

    update

--- apps/install.sh
+++ apps/install.sh
@@ -1,3 +1,4 @@
 echo "install apps on http://${1}:8080/v2/apps"
 
 curl -X POST -H "Content-Type: application/json" http://${1}:8080/v2/apps -d@./marathon-lb.json
+curl -X POST -H "Content-Type: application/json" http://${1}:8080/v2/apps -d@./marathon-lb-autoscale.json
