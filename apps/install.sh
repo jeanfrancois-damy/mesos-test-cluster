d8188e2c1abf4bd74ad598066d0d2d195a704c3f
jeff <jeff@studiogazoline.com>
2016-03-25 10:13:38 +0100

    ;)

--- apps/install.sh
+++ apps/install.sh
@@ -1,4 +1,5 @@
 echo "install apps on http://${1}:8080/v2/apps"
 
+#curl -X POST -H "Content-Type: application/json" http://${1}:8080/v2/apps -d@./mesos-dns.json
 curl -X POST -H "Content-Type: application/json" http://${1}:8080/v2/apps -d@./marathon-lb.json
-curl -X POST -H "Content-Type: application/json" http://${1}:8080/v2/apps -d@./marathon-lb-autoscale.json
+# curl -X POST -H "Content-Type: application/json" http://${1}:8080/v2/apps -d@./marathon-lb-autoscale.json
--- run.sh
+++
@@ -1,5 +0,0 @@
-export MASTER_IP=192.168.64.2
-export SLAVE_IP=192.168.64.2
-export MARATHON_IP=192.168.64.2
-rm -rf docker-compose.yml; 
-envsubst < "template.yml" > "docker-compose.yml";
